e18b575ba187da93108553b7ca2a18417d90e120
bin386@163.com <bin386@163.com>
2020-04-07 20:16:54 +0800

    新增redux-thunk依赖，store/index.js import redux-thunk,applyMiddleware(thunk)

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -7,7 +7,8 @@ import { fromJS } from 'immutable';
 
 // 定义header组件初始化store数据内容
 const defaultState = fromJS({
-    focused:false
+    focused:false,
+    list:[]
 });
 
 // reducer负责拿到原始的state的值，一定不能对state做修改，要返回新的state回去，
