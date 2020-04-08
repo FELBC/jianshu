bc5088119459892dab8b55f1acdbdc49dba4a601
bin386@163.com <bin386@163.com>
2020-04-08 12:33:07 +0800

    热门搜索换一批功能实现，actionCreators.js增加mouseEnter,mouseLeave,changePage三个action，reducer.js增加mouseIn,list,page,totalPage四个初始数据，switch增加对应action修改state的逻辑，index.js mapStateToProps增加从store inducer来getIn获取到的page，totalPage1,mouseIn,mapDispatchToProps增加mouserEnter,mouseLeave,changePage事件的dispatch,getListArea增加换一批分页更能及展示数据到页面相关判断逻辑

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -8,7 +8,10 @@ import { fromJS } from 'immutable';
 // 定义header组件初始化store数据内容
 const defaultState = fromJS({
     focused:false,
-    list:[]
+    mouseIn:false,
+    list:[],
+    page:1,
+    totalPage:1
 });
 
 // reducer负责拿到原始的state的值，一定不能对state做修改，要返回新的state回去，
@@ -26,7 +29,16 @@ export default (state = defaultState, action) => {
         case constants.SEARCH_BLUR:
             return state.set('focused',false);
         case constants.CHANGE_LIST:
-            return state.set('list',action.data);
+            return state.merge({
+                list:action.data,
+                totalPage:action.totalPage
+            });
+        case constants.MOUSE_ENTER:
+            return state.set('mouseIn',true);
+        case constants.MOUSE_LEAVE:
+            return state.set('mouseIn',false);
+        case constants.CHANGE_PAGE:
+            return state.set('page',action.page);
         default:
             return state;
     }
