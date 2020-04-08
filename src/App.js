71e5ec4e81bc854b97325943e8229774bb00648a
bin386@163.com <bin386@163.com>
2020-04-08 16:20:44 +0800

    新增react-router-dom依赖，App.js增加home，detail路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header';
 import store from './store';
 
@@ -9,7 +10,15 @@ class App extends Component{
       // Provider里面所有的组件都有能力去使用store里的数据
       // Provider把store里的数据都提供给了它内部的Header组件
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
