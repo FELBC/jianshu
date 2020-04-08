bd4f2ec040d459237714ce8117411e18af4fc547
bin386@163.com <bin386@163.com>
2020-04-08 17:49:19 +0800

    新增pages/home/components/list.js,Recomment.js,Topic.js,writer.js,新增pages/home/style.js,home index.js引入对应Topic,List,Recommend，Writer组件，组件在引入通过export default 类名方式导出的组件时，import 类名切记不要加大挂号，不然会报错(坑)

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import { 
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './style';
 
 class Home extends Component{
     render(){
         return(
-            <div>home</div>
+            <HomeWrapper>
+                <HomeLeft>
+                    <img 
+                        className='banner-img' 
+                        alt='' 
+                        src='https://upload-images.jianshu.io/upload_images/15684874-7428c129a17d0ae6.jpg?imageMogr2/auto-orient/strip|imageView2/2/w/1000/format/webp' 
+                    />
+                    <Topic />
+                    <List />
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend />
+                    <Writer />
+                </HomeRight>
+            </HomeWrapper>
         )
     }
 }
