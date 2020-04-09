76fd625c2d0b273df744c3b0dfbe6495ac740c6f
bin386@163.com <bin386@163.com>
2020-04-09 14:45:38 +0800

    新增public/api/home.json,home/store/reducer.js defaultState里面模拟数据迁移到home.json,home/index.js componentDidMount生命周期钩子并发送首页异步数据请求，并用connect连接store，reducer,，通过调用mapDispatch的this.props.changeHomeData dispatch action,并将type,异步请求到的数据一起发送给reducer，reducer接收到使用immutable state.merge方法更新store reducer里面数据

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -1,50 +1,19 @@
 import { fromJS } from 'immutable';
 
 const defaultState = fromJS({
-    topicList:[{
-        id:1,
-        title:'社会热点',
-        imgUrl:'https://upload.jianshu.io/collections/images/551468/crop1511949510763.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp'
-    },{
-        id:2,
-        title:'手绘',
-        imgUrl:'https://upload.jianshu.io/collections/images/1861725/crop1583541743141.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144/format/webp'
-    }],
-    articleList:[{
-        id:1,
-        title:'一个人是否值得交往，看这四点就够了',
-        desc:'有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。” 朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/3356997-6b687f4a1850c966.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    },
-    {
-        id:2,
-        title:'一个人是否值得交往，看这四点就够了',
-        desc:'有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。” 朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/3356997-6b687f4a1850c966.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    },
-    {
-        id:3,
-        title:'一个人是否值得交往，看这四点就够了',
-        desc:'有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。” 朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/3356997-6b687f4a1850c966.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    },
-    {
-        id:4,
-        title:'一个人是否值得交往，看这四点就够了',
-        desc:'有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。” 朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结...',
-        imgUrl:'https://upload-images.jianshu.io/upload_images/3356997-6b687f4a1850c966.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
-    }],
-    recommendList:[{
-        id:1,
-        imgUrl:'https://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png'
-    },{
-        id:2,
-        imgUrl:'https://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'
-    }]
+    topicList:[],
+    articleList:[],
+    recommendList:[]
 });
 
 export default (state=defaultState,action) => {
     switch(action.type){
+        case 'change_home_data':
+            return state.merge({
+                topicList:fromJS(action.topicList),
+                articleList:fromJS(action.articleList),
+                recommendList:fromJS(action.recommendList)
+            })
         default:
             return state;
     }
