2ff48acc4f333035a8aaff6a62bcbea94e0caa7a
bin386@163.com <bin386@163.com>
2020-04-09 17:25:28 +0800

    新增public/api/homeList.json更多文字模拟json，List.js新增LoadMore样式组件，通过点击更多文字调用mapDispatch getMoreList方法，getMoreList dispatch actionCreators.getMoreList,传入对应的请求页码，页码通过mapState渲染到页面方法获取，actionCreators调用getMoreList方法想后台发送异步数据分页请求，并dispatch addHomeList action，store，reducer根据action type处理逻辑，将请求到的新数据合并到原来的articleList,List.js组件mapState拿到数据重新渲染页面

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as constants from './constants';
+import { fromJS } from 'immutable';
 
 const changeHomeData = (result) => ({
     type:constants.CHANGE_HOME_DATA,
@@ -8,11 +9,27 @@ const changeHomeData = (result) => ({
     recommendList:result.recommendList
 })
 
+const addHomeList = (list,nextPage) => ({
+    type:constants.ADD_ARTICLE_LIST,
+    list:fromJS(list),
+    nextPage
+})
+
 export const getHomeInfo = () => {
     return (dispatch) => {
         axios.get('/api/home.json').then((res) => {
             const result = res.data.data;
             dispatch(changeHomeData(result));
+        })
+    }
+}
+
+// 真实数据请求需要分页往后台传页码,后台根据页码返回不同数据
+export const getMoreList = (page) => {
+    return (dispatch) => {
+        axios.get('/api/homeList.json?page=' + page).then((res) => {
+            const result = res.data.data;
+            dispatch(addHomeList(result,page+1));
         })
     }
 }
