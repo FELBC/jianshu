3dae449cf57e61433b1c3d676b87925670e3dd63
bin386@163.com <bin386@163.com>
2020-04-09 11:34:01 +0800

    pages/home/store/reducer.js新增recommendList数据，Recommend.js connect连接store reducer,通过mapState获取reducer的recommendList数据，并通过map循环渲染到页面展示，writer.js留作作业

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -33,6 +33,13 @@ const defaultState = fromJS({
         title:'一个人是否值得交往，看这四点就够了',
         desc:'有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。” 朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结...',
         imgUrl:'https://upload-images.jianshu.io/upload_images/3356997-6b687f4a1850c966.png?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240'
+    }],
+    recommendList:[{
+        id:1,
+        imgUrl:'https://cdn2.jianshu.io/assets/web/banner-s-club-aa8bdf19f8cf729a759da42e4a96f366.png'
+    },{
+        id:2,
+        imgUrl:'https://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'
     }]
 });
 
