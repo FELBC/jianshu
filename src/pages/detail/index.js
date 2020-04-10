225f9cdf34388fbd61cefad74ccfa76702ce76af
bin386@163.com <bin386@163.com>
2020-04-10 10:21:28 +0800

    新增/pages/detail/store/actionCreators.js,constants.js,index.js,reducer.js, store/reducer.js引入detailReducer并combineReducers,reducer.js增加detail组件的初始数据title，content，index.js通过connect连接store，reducer，通过mapState获取页面数据并绑定到页面，Content样式组件通过dangerouslySetInnerHTML嵌入字符串content页面内容

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,35 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component{
     render(){
         return(
             <DetailWrapper>
-                <Header>
-                    一个人是否值得交往，看这四点就够了
-                </Header>
-                <Content>
-                    <img alt='' src='https://upload-images.jianshu.io/upload_images/11864358-a0a516a8e7bbaa45?imageMogr2/auto-orient/strip|imageView2/2/w/640/format/webp' />
-                    <p>
-                        <b>有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。”
-                        朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结交的人。一个人是否值得交往，看这四点就够了。</b>
-                    </p>
-                    <p>
-                        有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。”
-                        朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结交的人。一个人是否值得交往，看这四点就够了。
-                    </p>
-                    <p>
-                        有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。”
-                        朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结交的人。一个人是否值得交往，看这四点就够了。
-                    </p>
-                    <p>
-                        有人说：“不必把太多人请进生命里，若他们走进不了你的内心，就只会把你的生命搅扰得拥挤不堪。”
-                        朋友不必多，贵在能知心。珍贵的情谊，要留给最值得结交的人。一个人是否值得交往，看这四点就够了。
-                    </p>
-                </Content>
+                <Header>{this.props.title}</Header>
+                <Content 
+                    dangerouslySetInnerHTML={{__html:this.props.content}} 
+                />
             </DetailWrapper>
         )
     }
 }
 
-export default Detail;
+const mapState = (state) => ({
+    title:state.getIn(['detail','title']),
+    content:state.getIn(['detail','content'])
+});
+
+export default connect(mapState,null)(Detail);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,12 +5,14 @@
 // 然后通过combineReducers再做整合
 import { combineReducers } from 'redux-immutable';
 import { reducer as headerReducer } from '../common/header/store';
-import { reducer as homeReducer} from '../pages/home/store';
+import { reducer as homeReducer } from '../pages/home/store';
+import { reducer as detailReducer } from '../pages/detail/store';
 
 //整合各模块对应reducer，组合成大的reducer
 const reducer = combineReducers({
     header: headerReducer,
-    home:homeReducer
+    home:homeReducer,
+    detail:detailReducer
 })
 
 export default reducer;
